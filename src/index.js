69fd31ae15f29d3de03b2e4a1c5b5747bcd2ec5c
sk-sergey <pikachu.from.ch@gmail.com>
2021-08-19 15:19:59 +0300

    feat: add min, max, avg function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
 
 exports.min = function min (array) {
-  
-  return 0;
+ 
+    if (!isValid(array)) return 0;
+
+    let minNumber
+    for (let i = 0; i < array.length; i++ ) {
+      minNumber == undefined ? minNumber = array[0] : {} ;
+      Number(minNumber) < Number(array[i]) ? {} : minNumber = array[i] ;
+  }
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if (!isValid(array)) return 0;
+  
+  let maxNumber
+  for (let i = 0; i < array.length; i++ ) {
+    maxNumber == undefined ? maxNumber = array[0] : {} ;
+    Number(maxNumber) > Number(array[i]) ? {} : maxNumber = array[i] ;
+  
+  }
+return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if (!isValid(array)) return 0;
+
+  let sum = 0
+
+  for (let i = 0; i < array.length; i++ ) {
+    sum += array[i]
+  }
+
+  return sum / (array.length);
+}
+
+function isValid(array) {
+  return Boolean(!(!Array.isArray(array) || array.length == 0))
 }
